78c04d396c165b7e2dd0dc5e35996266062c344a
Alex Huynh <abhuynh@iastate.edu>
2022-02-02 21:32:20 -0600

    Make first page have calendar, second page have first and last name as input, and third page have rating system

---
+++ Experiments/abhuynh/HelloWorld/app/src/main/java/com/example/helloworld/SecondActivity.java
@@ -0,0 +1,41 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+    }
+
+    public void sayHello(View view){
+        EditText firstNameInputLabel = (EditText) findViewById(R.id.firstNameInputLabel);
+        EditText lastNameInputLabel = (EditText) findViewById(R.id.lastNameInputLabel);
+        String firstName = firstNameInputLabel.getText().toString();
+        String lastName = lastNameInputLabel.getText().toString();
+        TextView nameTextView = findViewById(R.id.nameTextView);
+        if(firstName == "" && lastName == "")
+            nameTextView.setText("Please enter your name.");
+        else {
+            nameTextView.setText("Hello " + firstName + " " + lastName);
+        }
+    }
+
+    public void previousPage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void nextPage(View view) {
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
+    }
+}
