18b612cdcb7bad230e07709d76de2853965ec467
Matt Sinnwell <mattds@iastate.edu>
2022-02-03 18:57:18 -0600

    playing around with different endpoints

--- Experiments/mattds/Experiment1/cs309/src/main/java/com/isu/cs309/FirstController.java
+++ Experiments/mattds/Experiment1/cs309/src/main/java/com/isu/cs309/FirstController.java
@@ -1,19 +1,47 @@
 package com.isu.cs309;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 class FirstController {
 
+    User user1 = new User(
+            "Matt",
+            "Sinnwell",
+            "615 10th St. Ames, IA 50010",
+            "mattds@iastate.edu",
+            "505-400-5981");
+
+    User user2 = new User(
+            "John",
+            "Doe",
+            "615 10th St. Ames, IA 50010",
+            "jdoe@iastate.edu",
+            "123-456-7890");
+
     @GetMapping("/")
     public String welcome() {
-        return "This is Matt's Experimental Spring";
+        return "The TA should give Matt an A for this demo, it's awesome";
     }
 
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
-        return "Hello and welcome to COMS 309: " + name;
+        return "The person who made this endpoint is: " + name;
+    }
+
+    @GetMapping("/object")
+    public User returnObject() {
+        return user1;
+    }
+
+    @GetMapping("/objects")
+    public List<User> returnObjects() {
+        ArrayList<User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+        return list;
     }
 }
---
+++ Experiments/mattds/Experiment1/cs309/src/main/java/com/isu/cs309/User.java
@@ -0,0 +1,58 @@
+package com.isu.cs309;
+
+public class User {
+
+    String firstName;
+    String lastName;
+    String address;
+    String email;
+    String phoneNumber;
+
+    public User(String firstName, String lastName, String address, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
