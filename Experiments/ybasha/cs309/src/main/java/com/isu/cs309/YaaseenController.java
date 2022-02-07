174509cca9bc2cea4db77e2406906ac7150f667d
Yaaseen <ybasha@iastate.edu>
2022-02-06 21:49:32 -0600

    added path variable for testing

--- Experiments/ybasha/cs309/src/main/java/com/isu/cs309/YaaseenController.java
+++ Experiments/ybasha/cs309/src/main/java/com/isu/cs309/YaaseenController.java
@@ -7,9 +7,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class YaaseenController {
 
-    @GetMapping("/")
-    public String welcome() { return "Hello World! This is a test!";}
+//    @GetMapping("/")
+//    public String welcome() { return "Hello World! This is a test!";}
 
     @GetMapping("/food")
     public String welcome2() { return "It is cold.";}
+
+    @GetMapping("/")
+    public String welcome3() {
+        return "Hello and welcome to COMS 309";
+    }
+
+    @GetMapping("/{name}")
+    public String welcome(@PathVariable String name) {
+        return "Hello and welcome to COMS 309: " + name;
+    }
+
 }
