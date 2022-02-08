75da16382a0ed6cd58db36aa88b77f70f9f452a5
Yaaseen <ybasha@iastate.edu>
2022-02-07 23:57:39 -0600

    created a players class and player controller

--- Experiments/ybasha/tutorial3/hellopeople/src/main/java/coms309/players/PlayersController.java
+++ Experiments/ybasha/tutorial3/hellopeople/src/main/java/coms309/players/PlayersController.java
@@ -1,4 +1,60 @@
 package coms309.players;
 
+
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.HashMap;
+
+@RestController
 public class PlayersController {
+
+    HashMap<Integer, Players> playerList = new  HashMap<>();
+
+    Players mj = new Players("Michael", "Jordan", "Basketball", 1 );
+    Players phelps = new Players("Michael", "Phelps", "Swimming", 2 );
+    Players tiger = new Players("Tiger", "Woods", "Golf", 3 );
+
+    @PostMapping("/players")
+    public @ResponseBody String createPlayer(@RequestBody Players player) {
+        playerList.put(mj.getId(), mj);
+        playerList.put(phelps.getId(), phelps);
+        playerList.put(tiger.getId(), tiger);
+
+        System.out.println(player);
+        playerList.put(player.getId(), player);
+        return "New player "+ player.getFirstName() + " Saved";
+    }
+
+    @GetMapping("/players")
+    public @ResponseBody HashMap<Integer,Players> getAllPlayers() {
+        return playerList;
+    }
+
+    @GetMapping("/player/{id}")
+    public @ResponseBody Players getPlayer(@PathVariable Integer id) {
+        Players p = playerList.get(id);
+        return p;
+    }
+
+    @PutMapping("/player/{id}")
+    public @ResponseBody Players updatePlayer(@PathVariable Integer id, @RequestBody Players p) {
+        playerList.replace(id, p);
+        return playerList.get(id);
+    }
+
+    @DeleteMapping("/player/{id}")
+    public @ResponseBody HashMap<Integer, Players> deletePlayer(@PathVariable Integer  id) {
+        playerList.remove(id);
+        return playerList;
+    }
+
+    //testing with request parameter!
+    @PutMapping("/secondPlayer/{id}")
+    public @ResponseBody Players putInfo(@PathVariable Integer id, @RequestParam String fName) {
+        Players p = playerList.get(id);
+        p.setFirstName(fName);
+        return p;
+    }
+
 }
