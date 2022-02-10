72aa3395b89c950ec238a4628ab67af80f722c82
Alex Huynh <abhuynh@iastate.edu>
2022-02-10 16:40:12 -0600

    Add ability to sign in as a rider

--- Frontend/Registration/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Frontend/Registration/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signIn(View view){
+        EditText username = (EditText) findViewById(R.id.usernameInput);
+        EditText password = (EditText) findViewById(R.id.passwordInput);
+        if(username.getText().toString() == "rider" && password.getText().toString() == "password"){
+            Intent intent = new Intent(this, RiderHomePage.class);
+            startActivity(intent);
+        }
     }
 }
