7d149458fa37043cb6753c8dd42de7624e7152a9
Matt Sinnwell <mattds@iastate.edu>
2022-02-16 16:39:59 -0600

    BE-25 refactored classes and testing branches.

--- Backend/Backend/Backend/src/main/java/_HB_2/Backend/Driver.java
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/Driver.java
@@ -3,9 +3,6 @@ package _HB_2.Backend;
 public class Driver extends User{
     public Driver(String firstName, String lastName, String address, String state, String zip, String email, String phoneNumber) {
         super(firstName, lastName, address, state, zip, email, phoneNumber);
-    }
-
-    public Driver(int id, String firstName, String lastName, String address, String state, String zip, String email, String phoneNumber) {
-        super(id, firstName, lastName, address, state, zip, email, phoneNumber);
+        this.isADriver = true;
     }
 }
--- Backend/Backend/Backend/src/main/java/_HB_2/Backend/Rider.java
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/Rider.java
@@ -4,9 +4,7 @@ public class Rider extends User{
 
     public Rider(String firstName, String lastName, String address, String state, String zip, String email, String phoneNumber) {
         super(firstName, lastName, address, state, zip, email, phoneNumber);
+        this.isARider = true;
     }
 
-    public Rider(int id, String firstName, String lastName, String address, String state, String zip, String email, String phoneNumber) {
-        super(id, firstName, lastName, address, state, zip, email, phoneNumber);
-    }
 }
