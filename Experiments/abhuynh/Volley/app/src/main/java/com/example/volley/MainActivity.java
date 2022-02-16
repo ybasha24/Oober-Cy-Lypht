caf6d36223954f6321da5de53c8d3bbd48975f1a
Alex Huynh <abhuynh@iastate.edu>
2022-02-16 11:47:05 -0600

    Add JSON array get method to experiment

--- Experiments/abhuynh/Volley/app/src/main/java/com/example/volley/MainActivity.java
+++ Experiments/abhuynh/Volley/app/src/main/java/com/example/volley/MainActivity.java
@@ -5,6 +5,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -17,6 +18,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 
@@ -24,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
     private String tag_string_req = "string_req";
+    private String tag_object_req = "object_req";
+    private String tag_array_req = "array_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,21 +37,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void stringRequest(View view){
-        TextView textView = (TextView) findViewById(R.id.textview);
         String url = "https:api.androidhive.info/volley/string_response.html";
+        TextView textView = (TextView) findViewById(R.id.textview);
 
-        StringRequest req = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
-            public void onResponse(String response) {
-                Log.d(TAG, response.toString());
-                textView.setText(response.toString());
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
+        StringRequest req = new StringRequest(Request.Method.GET, url,
+            response -> {
+                Log.d(TAG, response);
+                textView.setText(response);
+            },
+            error -> {
                 VolleyLog.d(TAG, "Error: " + error.getMessage());
                 textView.setText("Error!");
             }
-        });
+        );
 
         AppController.getInstance().addToRequestQueue(req, tag_string_req);
 
@@ -57,20 +59,31 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.textview);
         String url = "https:api.androidhive.info/volley/person_object.json";
         JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d(TAG, response.toString());
-                        textView.setText(response.toString());
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        VolleyLog.d(TAG, "Error " + error.getMessage());
-                    }
-                });
-        AppController.getInstance().addToRequestQueue(req, tag_string_req);
+            response -> {
+                Log.d(TAG, response.toString());
+                textView.setText(response.toString());
+            },
+            error -> {
+                VolleyLog.d(TAG, "Error " + error.getMessage());
+                textView.setText("Error!");
+            }
+        );
+        AppController.getInstance().addToRequestQueue(req, tag_object_req);
+    }
 
+    public void jsonArrayRequest(View view){
+        TextView textView = (TextView) findViewById(R.id.textview);
+        String url = "https:api.androidhive.info/volley/person_array.json";
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, url, null,
+            response -> {
+                Log.d(TAG, response.toString());
+                textView.setText(response.toString());
+            },
+            error -> {
+                VolleyLog.d(TAG, "Error " + error.getMessage());
+                textView.setText("Error!");
+            }
+        );
+        AppController.getInstance().addToRequestQueue(req, tag_array_req);
     }
 }
