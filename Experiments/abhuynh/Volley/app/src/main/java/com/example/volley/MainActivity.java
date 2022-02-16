2d084fbbdf236057a781859184029823fb6531be
Alex Huynh <abhuynh@iastate.edu>
2022-02-16 11:35:20 -0600

    Add JSON object get method to experiment

--- Experiments/abhuynh/Volley/app/src/main/java/com/example/volley/MainActivity.java
+++ Experiments/abhuynh/Volley/app/src/main/java/com/example/volley/MainActivity.java
@@ -5,6 +5,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.volley.app.AppController;
@@ -15,6 +16,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -31,10 +34,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void stringRequest(View view){
         TextView textView = (TextView) findViewById(R.id.textview);
-
         String url = "https:api.androidhive.info/volley/string_response.html";
 
-        StringRequest strReq = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+        StringRequest req = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
             public void onResponse(String response) {
                 Log.d(TAG, response.toString());
                 textView.setText(response.toString());
@@ -47,7 +49,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+        AppController.getInstance().addToRequestQueue(req, tag_string_req);
+
+    }
+
+    public void jsonObjectRequest(View view){
+        TextView textView = (TextView) findViewById(R.id.textview);
+        String url = "https:api.androidhive.info/volley/person_object.json";
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        textView.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.d(TAG, "Error " + error.getMessage());
+                    }
+                });
+        AppController.getInstance().addToRequestQueue(req, tag_string_req);
 
     }
 }
