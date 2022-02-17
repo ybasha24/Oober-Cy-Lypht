4958cfa0ef77cf136e694587bb0302de42bab2f2
Matt Sinnwell <mattds@iastate.edu>
2022-02-16 21:55:20 -0600

    BE-24 Post a Driver with Hashmap as Data Table

--- Backend/Backend/Backend/src/main/java/_HB_2/Backend/DriverController.java
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/DriverController.java
@@ -1,2 +1,25 @@
-package _HB_2.Backend;public class DriverController {
+package _HB_2.Backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping( "/driver")
+public class DriverController {
+
+    @Autowired
+    private DriverService driverService;
+
+    @PostMapping("/registerDriver")
+    public @ResponseBody String saveDriver(
+            @RequestBody Driver d
+    ){
+        try{
+            driverService.save(d);
+            return "You successfully registered a Driver with ID# " + d.getId();
+        } catch (Exception e) {
+            return "there was a problem";
+        }
+    }
 }
--- Backend/Backend/Backend/src/main/java/_HB_2/Backend/DriverService.java
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/DriverService.java
@@ -1,2 +1,16 @@
-package _HB_2.Backend;public class DriverService {
+package _HB_2.Backend;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class DriverService {
+
+    public void save(Driver driver) {
+        HashMap<Long, User> mapOfUsers = new HashMap<>();
+
+        mapOfUsers.put(1L,driver);
+
+    }
 }
