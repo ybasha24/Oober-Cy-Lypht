3311d173fa3039aad87cfd7fcbd33a0cddcd4f60
Matt Sinnwell <mattds@iastate.edu>
2022-02-18 17:23:04 -0600

    BE-30 This H2 is impossible

--- Backend/Backend/Backend/src/main/java/_HB_2/Backend/DriverController.java
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/DriverController.java
@@ -4,22 +4,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping( "/driver")
+//@RestController
+//@RequestMapping( "/driver")
+//public class DriverController {
+//
+////    @Autowired
+////    private DriverService driverService;
+//
+//
+//    @Autowired
+//    DriverRepository driverRepository;
+//
+//
+//    @PostMapping("/registerDriver")
+//    String createDriver(
+//            @RequestBody Driver d
+//    )
+////    {
+////        try{
+////            driverService.createDriver(d);
+////            return "You successfully registered a Driver with ID# " + d.getId();
+////        } catch (Exception e) {
+////            return "there was a problem";
+////        }
+////    }
+//    {
+//        if (d == null) {
+//            return "There was an error";
+//        }
+//        //Driver driver = new Driver(d.firstName,d.lastName,d.address,d.state,d.zip,d.email,d.phoneNumber);
+//        driverRepository.save(d);
+//        return "You have succeeded";
+//    }
+//}
+
+@RestController
 public class DriverController {
-
     @Autowired
-    private DriverService driverService;
+    DriverRepository driverRepository;
 
-    @PostMapping("/registerDriver")
-    public @ResponseBody String createDriver(
-            @RequestBody Driver d
-    ){
-        try{
-            driverService.createDriver(d);
-            return "You successfully registered a Driver with ID# " + d.getId();
-        } catch (Exception e) {
-            return "there was a problem";
-        }
+    @PostMapping(path = "/driver/registerDriver")
+    String createUser(@RequestBody Driver d){
+        if (d == null)
+            return "You Didn't Make It";
+        driverRepository.save(d);
+        return "You are a hero";
     }
 }
