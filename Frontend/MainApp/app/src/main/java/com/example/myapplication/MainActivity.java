812622516070d751c04d9d6a797b80c86a2c11f0
Alex Huynh <abhuynh@iastate.edu>
2022-02-19 12:17:10 -0600

    Updates to home page for handling signing in

--- Frontend/MainApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Frontend/MainApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.myapplication.net_utils.AppController;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.sql.Driver;
 
@@ -18,16 +27,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void signIn(View view){
+    public void signIn(View view) throws JSONException {
         EditText usernameInput = (EditText) findViewById(R.id.usernameInput);
         EditText passwordInput = (EditText) findViewById(R.id.passwordInput);
         String username = usernameInput.getText().toString();
         String password = passwordInput.getText().toString();
-//        if(user == "rider" && pass == "rider"){
+
+        String url = "https://41096e03-1605-4363-a912-57afa92f86c7.mock.pstmn.io/bumbo";
+        JSONObject obj = new JSONObject();
+        obj.put("username", username);
+        obj.put("password", password);
+
+        boolean success = false;
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url, obj,
+                response ->{
+                    success = true;
+                    Log.d("JSON Object Request", "Success");
+                },
+                error ->{
+                    Log.d("JSON Object Request", "Error");
+                }
+        );
+        AppController.getInstance().addToRequestQueue(req, "post_object_tag")
+
+        if(success){
             Intent intent = new Intent(this, DriverHomePage.class);
             intent.putExtra("username", username);
             startActivity(intent);
-//        }
+        }
 //        else if(user == "driver" && pass == "driver"){
 //            Intent intent = new Intent(this, DriverHomePage.class);
 //            startActivity(intent);
