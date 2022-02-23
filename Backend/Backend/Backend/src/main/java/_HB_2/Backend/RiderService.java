1fb9be2bd67004ac24233e625bc86fa0caf83a58
Matt Sinnwell <mattds@iastate.edu>
2022-02-23 16:35:20 -0600

    What is going on

---
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/RiderService.java
@@ -0,0 +1,30 @@
+package _HB_2.Backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class RiderService {
+
+    @Autowired
+    RiderRepository riderRepository;
+
+    public void createRider(Rider rider) {
+
+        //set the rider flag
+        rider.isARider = true;
+        riderRepository.save(rider);
+    }
+
+    public User getRiderbyId(int id) {
+        return riderRepository.findById(id);
+    }
+
+    //public User getRiderbyEmail(String email) { return riderRepository.}
+
+    public void deleteRiderById(int id) {
+        riderRepository.deleteById(id);
+    }
+}
