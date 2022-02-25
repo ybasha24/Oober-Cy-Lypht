90a3b31c2b02361bc88c391167d39718648d9a1f
Matt Sinnwell <mattds@iastate.edu>
2022-02-25 14:45:15 -0600

    BE-46 Added Password attribute to User Class

--- Backend/Backend/Backend/src/main/java/_HB_2/Backend/Admin.java
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/Admin.java
@@ -5,8 +5,8 @@ import javax.persistence.Entity;
 @Entity
 public class Admin extends User{
 
-    public Admin(String firstName, String lastName, String address, String city, String state, String zip, String email, String phoneNumber) {
-        super(firstName, lastName, address, city, state, zip, email, phoneNumber);
+    public Admin(String firstName, String lastName, String address, String city, String state, String zip, String email, String phoneNumber, String password) {
+        super(firstName, lastName, address, city, state, zip, email, phoneNumber, password);
         this.isAnAdmin = true;
     }
 
