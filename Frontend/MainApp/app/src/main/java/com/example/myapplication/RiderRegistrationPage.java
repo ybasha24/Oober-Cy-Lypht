3ed40b85794e6296167db987b305172e1262dd49
Alex Huynh <abhuynh@iastate.edu>
2022-02-26 17:40:12 -0600

    Rebase test

--- Frontend/MainApp/app/src/main/java/com/example/myapplication/RiderRegistrationPage.java
+++ Frontend/MainApp/app/src/main/java/com/example/myapplication/RiderRegistrationPage.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -23,35 +24,52 @@ public class RiderRegistrationPage extends AppCompatActivity {
     }
 
     public void register(View view) throws JSONException {
-        EditText firstNameText = (EditText) findViewById(R.id.editTextFirstName);
-        String firstName = firstNameText.getText().toString();
+        TextView tv = ((TextView) findViewById(R.id.regStatusTextView));
+        String firstName = ((EditText) findViewById(R.id.editTextFirstName)).getText().toString();
         String lastName = ((EditText) findViewById(R.id.editTextLastName)).getText().toString();
         String email = ((EditText) findViewById(R.id.editTextEmail)).getText().toString();
-        String password = ((EditText) findViewById(R.id.passwordInput)).getText().toString();
-        String phone = ((EditText) findViewById(R.id.editTextPhone)).getText().toString();
+        String password = ((EditText) findViewById(R.id.editTextPassword)).getText().toString();
+        String phoneNumber = ((EditText) findViewById(R.id.editTextPhone)).getText().toString();
         String address = ((EditText) findViewById(R.id.editTextAddress)).getText().toString();
+        String city = ((EditText) findViewById(R.id.editTextCity)).getText().toString();
         String state = ((EditText) findViewById(R.id.editTextState)).getText().toString();
         String zip = ((EditText) findViewById(R.id.editTextZip)).getText().toString();
 
-        String url = "https://41096e03-1605-4363-a912-57afa92f86c7.mock.pstmn.io/create";
+        String url = "http://coms-309-030.class.las.iastate.edu:8080/rider/registerRider/";
         JSONObject obj = new JSONObject();
         obj.put("firstName", firstName);
         obj.put("lastName", lastName);
-        obj.put("email", email);
-        obj.put("password", password);
-        obj.put("phone", phone);
         obj.put("address", address);
+        obj.put("city", city);
         obj.put("state", state);
         obj.put("zip", zip);
+        obj.put("email", email);
+        obj.put("password", password);
+        obj.put("phoneNumber", phoneNumber);
 
         JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url, obj,
-            response ->{
-                ((TextView) findViewById(R.id.regStatusTextView)).setText("Success!");
-            },
-            error ->{
-                ((TextView) findViewById(R.id.regStatusTextView)).setText("Error!");
-            }
-        );
+                response -> {
+                    try {
+                        Intent intent = new Intent(this, RiderHomePage.class);
+                        String name = response.getString("firstName");
+                        boolean b = name == null;
+                        if(!response.isNull("firstName")){
+                            intent.putExtra("firstName", name);
+                            startActivity(intent);
+                            tv.setText(name);
+                        }
+                        else {
+                            tv.setText("Email already exists");
+                        }
+                    }
+                    catch(JSONException e){
+                        tv.setText("JSON Exception Error.");
+                    }
+                },
+                error -> {
+                    tv.setText("Please fill out each field.");
+                });
+
         AppController.getInstance().addToRequestQueue(req, "post_object_tag");
 
     }
