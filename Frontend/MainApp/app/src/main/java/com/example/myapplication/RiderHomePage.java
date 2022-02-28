b046051c25a99da516874d530cfc13dee4fcc656
Alex Huynh <abhuynh@iastate.edu>
2022-02-28 10:08:19 -0600

    Signing in works for riders

--- Frontend/MainApp/app/src/main/java/com/example/myapplication/RiderHomePage.java
+++ Frontend/MainApp/app/src/main/java/com/example/myapplication/RiderHomePage.java
@@ -7,15 +7,27 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class RiderHomePage extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rider_home_page);
 
-//        String username = getIntent().getStringExtra("username");
-//        TextView tv = (TextView) findViewById(R.id.welcomeBackTV);
-//        tv.setText("Welcome back " + username + "!");
+        String objString = getIntent().getStringExtra("obj");
+        JSONObject obj = null;
+        try{
+            obj = new JSONObject(objString);
+        } catch(JSONException e){}
+
+        TextView tv = (TextView) findViewById(R.id.welcomeBackTV);
+        try {
+            tv.setText("Welcome back " + obj.getString("firstName") + "!");
+        } catch(JSONException e){
+            tv.setText("Welcome back!");
+        }
     }
 }
