3bbd7391d0483d49208239dfcf6deebeb37f9c4c
Alex Huynh <abhuynh@iastate.edu>
2022-03-23 22:24:56 -0500

    Add ability to sign out and stay signed out

--- Frontend/MainApp/app/src/main/java/com/example/myapplication/driver/DriverHomePage.java
+++ Frontend/MainApp/app/src/main/java/com/example/myapplication/driver/DriverHomePage.java
@@ -3,6 +3,7 @@ package com.example.myapplication.driver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -30,6 +31,12 @@ public class DriverHomePage extends AppCompatActivity {
     }
 
     public void signOut(View view){
+        SharedPreferences.Editor editor = getSharedPreferences("name", MODE_PRIVATE).edit();
+        editor.putString("email", "");
+        editor.putString("password", "");
+        editor.putBoolean("isLoggedIn", false);
+        editor.apply();
+
         MainActivity.accountObj = null;
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
