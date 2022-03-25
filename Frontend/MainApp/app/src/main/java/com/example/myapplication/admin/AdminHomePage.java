76dc6670bca587aef67f837fcd2c10d02aecce61
Alex Huynh <abhuynh@iastate.edu>
2022-03-25 09:52:22 -0500

    Initial work on admin home page

---
+++ Frontend/MainApp/app/src/main/java/com/example/myapplication/admin/AdminHomePage.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myapplication.R;
+
+import android.os.Bundle;
+
+public class AdminHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home_page);
+    }
+}
