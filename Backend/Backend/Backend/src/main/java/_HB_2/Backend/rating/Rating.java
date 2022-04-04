9dbc0d12cfa32a40dd003c65fade98b3104552e2
Matt Sinnwell <mattds@iastate.edu>
2022-04-03 19:39:54 -0500

    BE-101 Created Rating Class

---
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/rating/Rating.java
@@ -0,0 +1,56 @@
+package _HB_2.Backend.rating;
+
+import _HB_2.Backend.user.User;
+
+import javax.persistence.*;
+
+@Entity
+public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "Rater_id")
+    User rater;
+
+    @ManyToOne
+    @JoinColumn(name = "Rated_id")
+    User rated;
+
+    int rating;
+
+    public Rating(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setRater(User rater) {
+        this.rater = rater;
+    }
+
+    public User getRater() {
+        return rater;
+    }
+
+    public void setRatee(User rated) {
+        this.rated = rated;
+    }
+
+    public User getRated() {
+        return rated;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+}
