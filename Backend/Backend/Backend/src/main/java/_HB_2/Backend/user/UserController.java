a555959c6ce4c13bff971ee51e8e8baac6fb1e50
Matt Sinnwell <mattds@iastate.edu>
2022-04-11 17:36:41 -0500

    BE-122 Added api documentation to User Controller Class

--- Backend/Backend/Backend/src/main/java/_HB_2/Backend/user/UserController.java
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/user/UserController.java
@@ -2,11 +2,14 @@ package _HB_2.Backend.user;
 
 import _HB_2.Backend.driver.Driver;
 import _HB_2.Backend.rider.Rider;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "UserController", description = "REST APIs related to user")
 @RestController
 @RequestMapping( "/user")
 public class UserController {
@@ -15,18 +18,21 @@ public class UserController {
     private UserService userService;
 
 
+    @ApiOperation(value = "Get a user by id", response = Iterable.class, tags = "getUser")
     @GetMapping("/getUser")
     User getUserById(@RequestParam int id) {
         User u = userService.getUserById(id);
         return u;
     }
 
+    @ApiOperation(value = "Get a user by email", response = Iterable.class, tags = "getUserByEmail")
     @GetMapping("/getUserByEmail")
     User getUserByEmail(@RequestParam String email) {
         User u = userService.getUserByEmail(email);
         return u;
     }
 
+    @ApiOperation(value = "Delete a user by Id", response = Iterable.class, tags = "deleteUser")
     @DeleteMapping("/deleteUser")
     String deleteUserById(
             @RequestParam int id) {
@@ -37,6 +43,7 @@ public class UserController {
     //Not sure why this doesn't work
     //Either delete this delete the function in the UserService, and create matching enpoints for Rider and Admin
     //Or fix and delete endpoint in DriverController and function in UserService
+    @ApiOperation(value = "Edit a user", response = Iterable.class, tags = "editUser")
     @PutMapping("/editUser")
     User editUser(
             @RequestParam int id,
@@ -46,6 +53,7 @@ public class UserController {
 
     //SIGN IN CHECKPOINT!!!
 
+    @ApiOperation(value = "Get a user by sign in info", response = Iterable.class, tags = "getUserSignIn")
     @GetMapping("/getUserSignIn")
     User getUserBySignIn(@RequestParam String email, String password) {
         User u = userService.getUserByEmail(email);
@@ -64,6 +72,7 @@ public class UserController {
         }
     }
 
+    @ApiOperation(value = "Get a list of all users", response = Iterable.class, tags = "getAllUsers")
     @GetMapping("/getAllUsers")
     List<User> getAllUsers() {
         return userService.getAllUsers();
