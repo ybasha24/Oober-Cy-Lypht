437db24b833ae94925d3c120b6766331221b5bfd
Yaaseen <ybasha@iastate.edu>
2022-04-12 14:10:44 -0500

    swagger api documentation

--- Backend/Backend/Backend/src/main/java/_HB_2/Backend/reports/ReportsController.java
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/reports/ReportsController.java
@@ -1,10 +1,13 @@
 package _HB_2.Backend.reports;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "ReportsController", description = "REST APIs related to Reports")
 @RestController
 @RequestMapping("/reports")
 public class ReportsController {
@@ -12,6 +15,7 @@ public class ReportsController {
     @Autowired
     ReportsService reportsService;
 
+    @ApiOperation(value = "Enter a new report", response = Reports.class)
     @PostMapping("/createReports")
     Reports createReports (@RequestParam int reporter_id,
                            @RequestParam int reported_id,
@@ -20,6 +24,7 @@ public class ReportsController {
         return reportsService.createReport(reporter_id, reported_id, reports);
     }
 
+    @ApiOperation(value = "Retrieve a list of reports for a given user", response = Iterable.class)
     @GetMapping("/getUserReports")
     List<Reports> getUserReports(@RequestParam int userId){
         return reportsService.getUserReports(userId);
