82039a46fad18c846c747295811938d42b1248d6
Alex Huynh <abhuynh@iastate.edu>
2022-04-14 11:28:10 -0500

    Initial design of trip details activity

---
+++ Frontend/MainApp/app/src/main/java/com/example/myapplication/driver/TripDetail.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.myapplication.*;
+
+public class TripDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_detail);
+    }
+}
