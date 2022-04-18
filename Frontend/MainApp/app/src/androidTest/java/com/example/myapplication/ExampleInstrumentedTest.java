17d34e56782f66e9013eae3d950195edbfcad43c
Alex Huynh <abhuynh@iastate.edu>
2022-04-18 16:41:08 -0500

    Successful test of logging in

--- Frontend/MainApp/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ Frontend/MainApp/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,14 +1,29 @@
 package com.example.myapplication;
 
-import android.content.Context;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.click;
 
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
+import static android.content.Context.MODE_PRIVATE;
+import com.example.myapplication.driver.HomePage;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +31,41 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+    private static final int SIMULATED_DELAY_MS = 500;
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+    @Rule
+    public ActivityScenarioRule<HomePage> activityRule2 = new ActivityScenarioRule<>(HomePage.class);
+
     @Test
     public void useAppContext() {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.myapplication", appContext.getPackageName());
     }
+
+
+    @Test
+    public void testLogin(){
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        SharedPreferences.Editor editor = appContext.getApplicationContext().getSharedPreferences("name", MODE_PRIVATE).edit();
+        editor.putString("email", "");
+        editor.putString("password", "");
+        editor.putBoolean("isLoggedIn", false);
+        editor.apply();
+
+        String email = "a";
+        String password = "a";
+        onView(withId(R.id.usernameInput))
+                .perform(typeText(email), closeSoftKeyboard());
+        onView(withId(R.id.passwordInput))
+                .perform(typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.loginButton)).perform(click());
+        try { Thread.sleep(SIMULATED_DELAY_MS); } catch (InterruptedException e) { }
+        onView(withText("Sign out")).check(matches(isDisplayed()));
+    }
+
+
 }
