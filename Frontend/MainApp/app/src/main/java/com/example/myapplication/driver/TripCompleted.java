2442f97dff247d6aebb8c0ded1ac768e9160168c
Alex Huynh <abhuynh@iastate.edu>
2022-04-22 09:40:44 -0500

    Initial implementation if driver rating screen

---
+++ Frontend/MainApp/app/src/main/java/com/example/myapplication/driver/TripCompleted.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.myapplication.*;
+
+public class TripCompleted extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_rate_rider);
+    }
+}
