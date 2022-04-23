32a5ae1f9e001a960470315c04ba439fbed799a5
Alex Huynh <abhuynh@iastate.edu>
2022-04-23 14:01:09 -0500

    Driver can now rate riders from 1-5

--- Frontend/MainApp/app/src/main/java/com/example/myapplication/driver/completetrip/RateRider.java
+++ Frontend/MainApp/app/src/main/java/com/example/myapplication/driver/completetrip/RateRider.java
@@ -2,33 +2,64 @@ package com.example.myapplication.driver.completetrip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.myapplication.*;
+import com.example.myapplication.app.AppController;
+import com.example.myapplication.driver.HomePage;
 import com.example.myapplication.driver.TripDetail;
+import com.example.myapplication.endpoints.Endpoints;
+
+import org.json.JSONObject;
 
 public class RateRider extends AppCompatActivity {
 
     TextView tv;
     EditText comments;
     RatingBar rating;
+    int driverId;
+    int riderId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_rate_rider);
+        try {
+            driverId = MainActivity.accountObj.getInt("id");
+            riderId = TripDetail.nameToIdMap.get(RateRidersAdapter.currentRiderString);
 
+        }catch(Exception e){}
         tv = findViewById(R.id.ratingNameTV);
-        tv.setText("Rating: " + RateRidersAdapter.currentRiderString + "; id = " + TripDetail.nameToIdMap.get(RateRidersAdapter.currentRiderString));
+        tv.setText("Rating: " + RateRidersAdapter.currentRiderString + "; id = " + riderId);
 
         comments = findViewById(R.id.driverCommentingRiderET);
         rating = findViewById(R.id.driverRatingRiderBar);
     }
 
-    public void submitRating(){
+    public void submitRating(View view){
+
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("rating", rating.getRating());
+        }catch(Exception e){}
+
+        String rateUrl = "http://coms-309-030.class.las.iastate.edu:8080/rating/createRating?raterId=5&ratedId=14";
+
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, rateUrl, obj,
+                response -> {
+                        Intent intent = new Intent(this, TripCompleted.class);
+                        startActivity(intent);
+                },
+                error -> runOnUiThread(() -> Toast.makeText(getApplicationContext(), "Error: " + error, Toast.LENGTH_LONG).show()));
+        AppController.getInstance().addToRequestQueue(req, "post_object_tag");
         //make 2 requests
     }
 }
