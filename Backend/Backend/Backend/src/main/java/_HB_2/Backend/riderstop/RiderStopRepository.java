2911ce1b5162dffa94895ebce5fa0d755db5500f
Matt Sinnwell <mattds@iastate.edu>
2022-04-24 23:00:01 -0500

    BE-143 giving up for the night.

--- Backend/Backend/Backend/src/main/java/_HB_2/Backend/riderstop/RiderStopRepository.java
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/riderstop/RiderStopRepository.java
@@ -2,6 +2,7 @@ package _HB_2.Backend.riderstop;
 
 import _HB_2.Backend.trip.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,8 +17,13 @@ public interface RiderStopRepository extends JpaRepository<RiderStop, Integer> {
     @Transactional
     void deleteById(int id);
 
-    @Query("DELETE FROM RiderStop r where r.tripId = :tripId and r.riderId = :riderId")
-    void deleteByTripIdAndRiderId(int tripId, int riderId);
+//    @Modifying
+//    @Query("DELETE FROM RiderStop r where r.tripId = :tripId and r.riderId = :riderId")
+//    void deleteByTripIdAndRiderId(int tripId, int riderId);
+
+    @Modifying
+    @Query("UPDATE RiderStop r set r.tripId = 0 where r.tripId = :tripId and r.riderId = :riderId")
+    int deleteByTripIdAndRiderId(int tripId, int riderId);
 
     @Query("SELECT r FROM RiderStop r WHERE r.tripId = :tripId")
     List<RiderStop> getRiderStopsByTripId(int tripId);
--- Backend/Backend/Backend/src/main/java/_HB_2/Backend/riderstop/RiderStopService.java
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/riderstop/RiderStopService.java
@@ -1,5 +1,6 @@
 package _HB_2.Backend.riderstop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,20 +9,22 @@ import java.util.List;
 @Service
 public class RiderStopService {
 
+    @Autowired
     private RiderStopRepository riderStopRepository;
 
     public List<RiderStop> getRiderStopsByTripId(int tripId) {
 
-        List<RiderStop> allRiderStops = riderStopRepository.findAll();
-        List<RiderStop> riderStopsForThisTrip = new ArrayList<>();
+//        List<RiderStop> allRiderStops = new ArrayList<>();
+//        allRiderStops = riderStopRepository.findAll();
+//        List<RiderStop> riderStopsForThisTrip = new ArrayList<>();
+//
+//        for(RiderStop riderStop : allRiderStops) {
+//            if (riderStop.getTripId() == tripId) {
+//                riderStopsForThisTrip.add(riderStop);
+//            }
+//        }
 
-        for(RiderStop riderStop : allRiderStops) {
-            if (riderStop.getTripId() == tripId) {
-                riderStopsForThisTrip.add(riderStop);
-            }
-        }
-
-        //        List<RiderStop> riderStopsForThisTrip = riderStopRepository.getRiderStopsByTripId(tripId);
+        List<RiderStop> riderStopsForThisTrip = riderStopRepository.getRiderStopsByTripId(tripId);
         return riderStopsForThisTrip;
     }
 }
