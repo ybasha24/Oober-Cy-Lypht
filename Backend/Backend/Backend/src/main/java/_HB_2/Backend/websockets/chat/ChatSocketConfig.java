ee211d7259a45afa09719c447a95bb555d56e53b
Yaaseen <ybasha@iastate.edu>
2022-04-26 18:27:32 -0500

    figuring out why pipeline is failing:

--- Backend/Backend/Backend/src/main/java/_HB_2/Backend/websockets/chat/ChatSocketConfig.java
+++ Backend/Backend/Backend/src/main/java/_HB_2/Backend/websockets/chat/ChatSocketConfig.java
@@ -3,7 +3,7 @@ package _HB_2.Backend.websockets.chat;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
+//error here?
 @Configuration
 public class ChatSocketConfig {
     @Bean
